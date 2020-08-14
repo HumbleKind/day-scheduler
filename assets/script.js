5c236614e52ca8da607634c929961c376b683566
Christian Sinai <christian_sinai@me.com>
2020-08-14 13:42:47 -0700

    added js time block loop

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+// group all code here to run when DOM is ready
+
+    var timeBlock = $(".time-block");
+    // console.log(timeBlock);
+
+    timeBlock.each(function() {
+    console.log($(this));
+    })
+
+});
