aff54e057ab5053657c077fef1ea9fc25d01346a
Christian Sinai <christian_sinai@me.com>
2020-08-14 14:05:20 -0700

    added time block current hour conditional

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,25 @@ $(document).ready(function(){
     var timeBlock = $(".time-block");
     // console.log(timeBlock);
 
+    var currentHour = moment().format('HH');
+
     timeBlock.each(function() {
-    console.log($(this));
+    
+    // NOTE: review the following ...
+    // console.log($(this));
+    // console.log(this.innerText);
+    // console.log($(this).attr("data-hour"));
+    // console.log($(this).data("hour"));
+
+        var thisHour = parseInt($(this).data("hour"));
+
+        if (thisHour == currentHour) {
+            // change time-block div class to 'present'
+        } else if (thisHour < currentHour) {
+            // change time-block div class to 'past'
+        } else if (thisHour > currentHour) {
+            // change time-block div class to 'future'
+        }
     })
 
 });
