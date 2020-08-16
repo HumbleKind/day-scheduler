a086c2b2ac4362adc9e4c20ce5e3d4561fea7b97
Christian Sinai <christian_sinai@me.com>
2020-08-15 21:48:44 -0700

    updated time block conditional

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,34 @@
 $(document).ready(function(){
 // group all code here to run when DOM is ready
 
+    var currentHour = moment().format('HH');
+
     var timeBlock = $(".time-block");
     // console.log(timeBlock);
 
-    var currentHour = moment().format('HH');
+    $("#current-day").text(moment().format('dddd[,] LL'));
 
     timeBlock.each(function() {
-    
-    // NOTE: review the following ...
-    // console.log($(this));
-    // console.log(this.innerText);
-    // console.log($(this).attr("data-hour"));
-    // console.log($(this).data("hour"));
 
-        var thisHour = parseInt($(this).data("hour"));
+        // NOTE: review the following ...
+        // console.log(this); // the element itself ... grabs entire html time-block div (current scope)
+        // console.log($(this)); // jQuery wrapper-object around the element
+        // console.log($(this).attr("data-hour"));
+        // console.log($(this).data("hour"));
+    
+        var thisHour = 10; //parseInt($(this).data("hour"));
+
+        $(this).removeClass("past present future");
 
         if (thisHour == currentHour) {
             // change time-block div class to 'present'
+            $(this).addClass("present");
         } else if (thisHour < currentHour) {
             // change time-block div class to 'past'
+            $(this).addClass("past");
         } else if (thisHour > currentHour) {
             // change time-block div class to 'future'
+            $(this).addClass("future");
         }
     })
 
