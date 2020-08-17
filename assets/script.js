ceafdbbd87a84f851fa1cb96658d2c8acaec5ab7
Christian Sinai <christian_sinai@me.com>
2020-08-17 09:06:04 -0700

    updated notes

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
 // group all code here to run when DOM is ready
 
+    // variable for the moment.js time information
     var currentHour = moment().format('HH');
     // console.log(currentHour);
 
+    // variable to get the time block divs
     var timeBlock = $(".time-block");
     // console.log(timeBlock);
 
+    // populates the jumbotron with the moment.js day/ time information
     $("#current-day").text(moment().format('dddd[,] LL'));
 
+    // the following function cycles through each time block
     timeBlock.each(function() {
 
         // NOTE: review the following ...
@@ -17,8 +21,10 @@ $(document).ready(function(){
         // console.log($(this).attr("data-hour"));
         // console.log($(this).data("hour"));
     
+        // local variable for the time block hour value (i.e. 10)
         var thisHour = parseInt($(this).data("hour"));
 
+        // removes either past, present, and/ or future class from time block div
         $(this).removeClass("past present future");
 
         if (thisHour == currentHour) {
